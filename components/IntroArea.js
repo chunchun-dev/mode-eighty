86da1fd13e7cee493a5223655a3ed7b31d233adb
qt-coder <chuchenkhang@gmail.com>
2021-10-20 08:50:47 +0800

    Added animation to into area

--- components/IntroArea.js
+++ components/IntroArea.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import styles from '../styles/IntroArea.module.css'
+import {motion} from 'framer-motion'
 
 function IntroArea() {
     return (
         <div id={styles.introArea}>
-            <h1 id={styles.title}>The Mode</h1>
-            <h1 id={styles.eighty}>Eighty</h1>
+            <motion.h1 
+                id={styles.title}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 50,
+                    delay: 0.25
+                }}>
+                    The Mode
+            </motion.h1>
+            <motion.h1 
+                id={styles.eighty}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 50,
+                    delay: 0.45
+                }}>
+                    Eighty
+            </motion.h1>
         </div>
     )
 }
