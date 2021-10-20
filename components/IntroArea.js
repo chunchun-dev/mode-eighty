2d82c7cd0ef03b25010b8d2d476913bc5aa98d98
qt-coder <chuchenkhang@gmail.com>
2021-10-20 08:11:31 +0800

    Intro Area

---
+++ components/IntroArea.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/IntroArea.module.css'
+
+function IntroArea() {
+    return (
+        <div id={styles.introArea}>
+            <h1 id={styles.title}>The Mode</h1>
+            <h1 id={styles.eighty}>Eighty</h1>
+        </div>
+    )
+}
+
+export default IntroArea
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import DescriptionArea from '../components/DescriptionArea'
+import IntroArea from '../components/IntroArea'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <IntroArea/>
       <DescriptionArea/>
     </div>
   )
